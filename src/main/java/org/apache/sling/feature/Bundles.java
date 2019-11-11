5531dd7cb8851a3ab184ec068c161faeea5ad4f3
Haris Adzemovic <adzemovic.haris@gmail.com>
2019-11-11 13:26:23 +0100

    Use Integer.comapreTo() for Integer comparison
    Made after discussion in previous PR:
    https://github.com/apache/sling-org-apache-sling-scripting-jsp/pull/2

--- src/main/java/org/apache/sling/feature/Bundles.java
+++ src/main/java/org/apache/sling/feature/Bundles.java
@@ -42,7 +42,7 @@ public class Bundles extends Artifacts {
 
             @Override
             public int compare(final Integer o1, final Integer o2) {
-                if ( o1.equals(o2) ) {
+                if ( o1.compareTo(o2) == 0 ) {
                     return 0;
                 }
                 if ( o1 == 0 ) {
